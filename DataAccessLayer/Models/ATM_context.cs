7d4143f4c15ce89fecda8704e6f315349da6343a
Gustavo Alpizar Herrera <gah18_94@hotmail.es>
2017-05-24 18:02:59 -0600

    .

---
+++ DataAccessLayer/Models/ATM_context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ATM_API.Models;
+
+namespace DataAccessLayer.Models
+{
+    public class ATM_context : DbContext
+
+    {
+        public ATM_context()
+        {
+
+        }
+
+        public DbSet<UserModel> User { get; set; }
+        public DbSet<AccountDetailsModel> AccountDetails { get; set; }
+        public DbSet<TransactionHistoryModel> TransacionHistory{ get; set; }
+
+    }
+}
--- src/ATM_API/Startup.cs
+++ src/ATM_API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using DataAccessLayer.Models;
 
 namespace ATM_API
 {
@@ -16,6 +17,9 @@ namespace ATM_API
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_config);
+
+            services.AddDbContext<ATM_context>;
             services.AddMvc();
         }
 
@@ -24,11 +28,11 @@ namespace ATM_API
         {
             loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
             app.UseStatusCodePages();
             app.UseMvc();
 
